fbe80611bcef53f5883237f6d43fd0fe9f19a7a5
Brian Blanco <brianblanco1@gmail.com>
2024-09-03 15:11:37 -0400

    Initial project

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Brian Blanco on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
